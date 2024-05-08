781937d32c0e25883c42355951a41475fd8bf7cc
Santiago Rodriguez <santiagorrodriguez9@gmail.com>
2024-05-07 22:58:57 -0500

    Primer cambio

---
+++ valores.js
@@ -0,0 +1,12 @@
+40
+
+"Santiago Rodriguez"
+
+true
+false
+
+null
+undefined
+
+[1, 2, 3] 
+{nombre: "Santiago" }
---
+++ variables.js
@@ -0,0 +1,18 @@
+var nombre = "Santiago";
+
+// declarar
+var edad;
+
+//inicializar
+edad = 30;
+
+edad 
+
+var elementos = ["Computadora", "Celular"];
+
+var persona = {
+    nombre: 'Santi',
+    edad: 30
+}
+
+persona;
