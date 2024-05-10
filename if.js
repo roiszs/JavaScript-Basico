e41fb0b0c8179b9889f5f780440fe9c7031aa44b
Santiago Rodriguez <santiagorrodriguez9@gmail.com>
2024-05-10 18:49:14 -0500

    Cambios en el curso

---
+++ if.js
@@ -0,0 +1,17 @@
+var edad = 18;
+
+if(edad === 18) {
+   console.log("Puedes votar, sera tu primera votación.")
+} else if (edad > 18) {
+   console.log("Puedes votar de nuevo")
+} else {
+    console.log("Aun no puedes votar")
+}
+
+
+
+condition ? true : false;
+
+var numero = 1;
+
+var resultado = numero === 1 ? "Sí soy un uno" : "No,no soy uno";
