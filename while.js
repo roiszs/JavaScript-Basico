c9ea6a8ed4335139a7214dc0d05627a7f6675509
Santiago Rodriguez <santiagorrodriguez9@gmail.com>
2024-05-14 16:43:35 -0500

    Agregamos los ciclos for y while

---
+++ while.js
@@ -0,0 +1,15 @@
+var estudiantes = ["Maria", "Sergio", "Rosa", "Daniel"];
+
+function saludarEstudiantes(estudiante) {
+    console.log(`Hola, ${estudiante}`);
+}
+
+while(estudiantes.length > 0) {
+    var estudiante = estudiantes.shift();
+    saludarEstudiantes(estudiante);
+}
+
+// Hola, Maria
+// Hola, Sergio
+// Hola, Rosa
+// Hola, Daniel
